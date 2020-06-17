fe9cc6cdc62050f9f97879a3a83cf20d5cb82fbb
chenyinjie <1761038183@qq.com>
2020-06-17 13:53:54 +0800

    tabbar

--- testvuecli2/src/App.vue
+++ testvuecli2/src/App.vue
@@ -1,66 +1,22 @@
 <template>
   <div id="app">
-    <h1>路由相关知识点</h1>
-    <!-- <router-link to="/home" tag="button" replace>首页</router-link>
-    <router-link to="/about" replace>关于</router-link> -->
-    <!-- <button @click="homeClick">首页</button>
-    <button @click="aboutClick">关于</button> -->
-
-    <router-link to='/home'>首页</router-link>
-    <router-link to='/about'>关于</router-link>
-    <router-link :to="'/user/'+userId">用户</router-link>
-    <!-- <router-link to="/profile">档案</router-link> -->
-    <!-- <router-link :to="{path:'/profile',
-    query:{
-      name:'DGM',
-      age:18,
-      height:1.88
-    }
-    }">档案</router-link> -->
-    <!-- 用按钮点击传递 -->
-    <button @click="profileClick">档案</button>
-    <keep-alive exclude="Profile,User ">
-      <router-view></router-view>
-    </keep-alive>
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
+  import MainTabBar from '../src/components/MainTabBar'
+
 export default {
   name: 'App',
-  data(){
-    return{
-      userId:"lisi"
-    }
-  },
-  methods: {
-    //通过代码的方式修改路由 vue-router
-    //push => pushState
-
-    homeClick(){
-      this.$router.push('/home')
-      // this.$router.replace('/home')
-    },
-    aboutClick(){
-      this.$router.push('/about')
-      // this.$router.replace('/about')
-    },
-    profileClick(){
-      this.$router.push({
-        path:'/profile',
-        query:{
-          name:'DGM',
-          age:20,
-          height:1.78
-        }
-      })
-    }
-  },
+  components:{
+   MainTabBar
+  }
 }
 </script>
 
 <style>
-  .router-link-active{
-    color: aquamarine;
-  }
+  @import url('./assets/css/base.css');
+
 </style>
---
+++ testvuecli2/src/components/tabbar/Tabbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Tabbar'
+}
+</script>
+
+<style>
+#tab-bar{
+    display: flex;
+    background-color: #f6f6f6;
+    /* 定位到底部 */
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    /* 设置顶部阴影 */
+    box-shadow: 0 -2px 1px rgba(100, 100, 100, 0.2);
+  }
+
+</style>
