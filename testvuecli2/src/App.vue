86169d64ee965299e56e4b254e94d4146f120c02
chenyinjie <1761038183@qq.com>
2020-06-18 22:41:45 +0800

    vuex

--- testvuecli2/src/App.vue
+++ testvuecli2/src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div id="app">
+    <h2>-----App:modules中内容-----</h2>
+    <h2>{{$store.state.a.name }}</h2>
+    <button @click="updateName">修改名字</button>
+    <h2>{{$store.getters.fullName}}</h2>
+    <h2>{{$store.getters.fullNameTwo}}</h2>
+    <h2>{{$store.getters.fullNameThree}}</h2>
+    <button @click="asyncUpdatename">异步修改名字</button>
+
     <h2>{{message}}</h2>
     <h2>{{$store.state.counter}}</h2>
     <button @click="addition">+</button>
     <button @click="subtraction">-</button>
+
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+
+    <h2>{{$store.state.info}}</h2>
+    <button @click="changeInfo">修改info</button>
+
+
+
+    <h2>--App内容：getters相关信息--</h2>
+    <h2>{{$store.getters.powerCounter}}</h2>
+    <h2 v-for="item in $store.getters.more20stu" :key="item.id">{{item.name}}</h2>
+    <h2>{{$store.getters.more20stuLength}}</h2>
+    <h2 v-for="item in $store.getters.moreAgeStu(1)" :key="item.name">{{item.name}}</h2>
+
     <hello-vuex/>
   </div>
 </template>
@@ -27,6 +50,46 @@ export default {
     },
     subtraction(){
       this.$store.commit('decrement')
+    },
+    addCount(count){
+      // Payload : 负载
+      // 1.普通的提交风格
+      // this.$store.commit('incrementCount',count)
+
+      // 2，特殊的提交风格
+      this.$store.commit({
+        type:'incrementCount',
+        count
+      })
+    },
+    changeInfo(){
+      // this.$store.commit('updateInfo')
+      // this.$store.dispatch('aUpdateInfo','this is payload')
+      // 参数传函数
+      // this.$store.dispatch('aUpdateInfo', () => {
+      //   console.log('里面成功执行');
+      // })
+      // this.$store.dispatch('aUpdateInfo',{
+      //   message:'我是携带的信息',
+      //   success: () => {
+      //     console.log('里面执行成功');
+
+      //   }
+      // })
+      this.$store
+      .dispatch('aUpdateInfo','我是携带的信息')
+      .then(res => {
+        console.log('里面执行成功');
+
+        console.log(res);
+
+      })
+    },
+    updateName(){
+      this.$store.commit('updateName','Lacsa')
+    },
+    asyncUpdatename(){
+      this.$store.dispatch('aUpdateName')
     }
   },
 }
--- testvuecli2/src/store/index.js
+++ testvuecli2/src/store/index.js
@@ -4,27 +4,116 @@ import Vuex from 'vuex'
 // 1.安装插件
 Vue.use(Vuex)
 
+
+const moduleA = {
+  state:{
+    name:'Tom'
+  },
+  mutations:{
+    updateName(state,payload){
+      state.name = payload
+    }
+  },
+  getters:{
+    fullName(state){
+      return state.name + ' one'
+    },
+    fullNameTwo(state,getters){
+      return getters.fullName + ' two'
+    },
+    fullNameThree(state,getters,rootState){
+      return getters.fullNameTwo + 'three' + rootState.counter
+    }
+  },
+  actions:{
+    aUpdateName(context){
+      setTimeout(() => {
+        console.log(context);
+
+        context.commit('updateName','Jack')
+      }, 2000);
+    }
+  },
+  modules:{}
+}
+
+
 // 2.创建对象
 const store = new Vuex.Store({
+  // 单一状态树
   state:{
-    counter:1000
+    counter:1000,
+    students:[
+      {id:110,name:'Chen',age:18},
+      {id:111,name:'Dong',age:24},
+      {id:112,name:'Wang',age:40},
+      {id:113,name:'Lin',age:10}
+    ],
+    info:{
+      name:'kobe',
+      age:40,
+      height:1.98
+    }
   },
+  // 状态跟新 commit  不能写异步操作
   mutations:{
     increment(state){
       state.counter++
     },
     decrement(state){
       state.counter--
+    },
+    incrementCount(state,Payload){
+      state.counter += Payload.count
+    },
+    updateInfo(state){
+      state.info.name = 'change'
     }
   },
+  // 类似mutations 用来记录异步操作
   actions:{
+    // context 上下文
+    // aUpdateInfo(context,payload){
+    //   setTimeout(() =>{
+    //     context.commit('updateInfo')
+    //     console.log(payload.message);
+    //     payload.success();
 
+    //   },2000)
+    // }
+    aUpdateInfo(context,payload){
+      return new Promise((resolve,reject) => {
+        setTimeout(() => {
+          context.commit('updateInfo')
+          console.log(payload);
+          resolve('111111')
+        }, 2000);
+      })
+    }
   },
+  // 类似于计算属性
   getters:{
-
+    powerCounter(state){
+      return state.counter * state.counter
+    },
+    more20stu(state){
+      return state.students.filter(s => s.age > 20)
+    },
+    more20stuLength(state,getters){
+      return getters.more20stu.length
+    },
+    moreAgeStu(state){
+      // return function(age){
+      //   return state.students.filter(s => s.age > age)
+      // }
+      return age => {
+        return state.students.filter(s => s.age > age)
+      }
+    }
   },
+  // 模块
   modules:{
-
+    a:moduleA
   }
 })
 
